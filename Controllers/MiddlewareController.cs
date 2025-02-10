670a6ec8f1580f2b6e333a9c49a8b776fd4ded2d
rawanalbenni <rawan.albenni@icloud.com>
2025-02-10 22:38:54 +0300

    Add files via upload

---
+++ Controllers/MiddlewareController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using support_task4.middlewares;
+
+namespace support_task4.Controllers
+{
+    public class MiddlewareController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Middleware.Paths);
+        }
+    }
+}
---
+++ middlewares/Middleware.cs
@@ -0,0 +1,19 @@
+﻿namespace support_task4.middlewares
+{
+    public class Middleware
+    {
+        private RequestDelegate _next;
+        public static List<String>Paths = new List<String>();
+
+        public Middleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context) {
+            Paths.Add(context.Request.Path);
+            await _next(context);
+        }
+     
+       
+    }
+}
